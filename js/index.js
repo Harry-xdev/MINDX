81363e3f4550786436127a13d2f0b3bca41f01a3
harry-xdev <harry.xdev@gmail.com>
2022-01-23 23:37:28 +0700

    fix

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ function render_header() {
         <a class="native" href="#">Women</a>
         <a class="native" href="#">About us</a>
         <a class="native" href="#">Wishlist</a>
-        <span id="user__container"><a href="#">Login</a><a href="#">Register</a></span>
+        <a id="user__container" href="#">Login</a><a href="#">Register</a>
         `;
     };
     render_header_native();
@@ -28,6 +28,8 @@ function render_main_section () {
     `;
 
     // RENDER SECTION ITEMS
+
+    // SECTION 1: THỦY TIÊN
     const section1 = document.querySelector('#section__container');
     function render_section_1() {
         section1.innerHTML += `
@@ -36,6 +38,7 @@ function render_main_section () {
     };
     render_section_1();
 
+    // SECTION 2: TRUNG
     const section2 = document.querySelector('#section__container');
     function render_section_2() {
         section2.innerHTML += `
@@ -44,6 +47,7 @@ function render_main_section () {
     };
     render_section_2();
 
+    // SECTION 3: ĐÔNG
     const section3 = document.querySelector('#section__container');
     function render_section_3() {
         section3.innerHTML += `
@@ -52,13 +56,61 @@ function render_main_section () {
     };
     render_section_3();
 
+    // SECTION 4: TUẤN ANH
     const section4 = document.querySelector('#section__container');
     function render_section_4() {
         section4.innerHTML += `
-        <section id="" class="section__4">Section 4</section>
+        <section id="" class="section__4"></section>
         `;
     };
     render_section_4();
+
+    // RENDER SECTION 4 ITEMS
+    const section4Item1 = document.querySelector('.section__4');
+    function section4__container1() {
+        section4Item1.innerHTML += `
+        <div id="section4__container1" class="">Handbag</div>
+        `;
+    };
+    section4__container1();
+
+    const section41item = document.querySelector('#section4__container1');
+    function section4__container1_item() {
+        section41item.innerHTML += `
+        <div id="" class="handbag__product">Products</div>
+        `;
+    };
+    section4__container1_item();
+
+    // CREATING PRODUCT ARRAY OBJECTS
+    const handbagProductList = [
+        {
+            name: "KAMPUS TIGER BACKPACK",
+            price: "$265",
+            gender: "MALE",
+            color: "BLACK",
+        },
+        {
+            name: "KENZO SPORT BELT BAG",
+            price: "$275",
+            gender: "MALE",
+            color: "BLACK",
+        },
+        {
+            name: "KENZO Sport backpack with strap",
+            price: "$295",
+            gender: "MALE",
+            color: "WHITE",
+        },
+        {
+            name: "KENZO Sport tote bag",
+            price: "$340",
+            gender: "MALE",
+            color: "BLACK",
+        },
+    ];
+
+
     
 };
 render_main_section();
