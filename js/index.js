ad3ef96ed9e87572da506be09742415ab543c055
harry-xdev <harry.xdev@gmail.com>
2022-01-24 22:13:07 +0700

    section4

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const mainPage = document.querySelector('#mainPage');
 function render_header() {
     mainPage.innerHTML += `
     <div id="header"></div>
-    `;
+    `
 
     // RENDER HEADER NATIVE BUTTON - USER LOGIN
     const headerNative = document.querySelector('#header');
@@ -14,18 +14,18 @@ function render_header() {
         <a class="native" href="#">Women</a>
         <a class="native" href="#">About us</a>
         <a class="native" href="#">Wishlist</a>
-        <a id="user__container" href="#">Login</a><a href="#">Register</a>
-        `;
-    };
+        <span id="user__container"><a href="#">Login</a><a href="#">Register</a></span>
+        `
+    }
     render_header_native();
-};
+}
 render_header();
 
 // RENDER MAIN SECTION
 function render_main_section () {
     mainPage.innerHTML += `
     <div id="section__container"></div>
-    `;
+    `
 
     // RENDER SECTION ITEMS
 
@@ -34,8 +34,8 @@ function render_main_section () {
     function render_section_1() {
         section1.innerHTML += `
         <section id="" class="section__1">Section 1</section>
-        `;
-    };
+        `
+    }
     render_section_1();
 
     // SECTION 2: TRUNG
@@ -43,8 +43,8 @@ function render_main_section () {
     function render_section_2() {
         section2.innerHTML += `
         <section id="" class="section__2">Section 2</section>
-        `;
-    };
+        `
+    }
     render_section_2();
 
     // SECTION 3: ĐÔNG
@@ -52,8 +52,8 @@ function render_main_section () {
     function render_section_3() {
         section3.innerHTML += `
         <section id="" class="section__3">Section 3</section>
-        `;
-    };
+        `
+    }
     render_section_3();
 
     // SECTION 4: TUẤN ANH
@@ -61,58 +61,80 @@ function render_main_section () {
     function render_section_4() {
         section4.innerHTML += `
         <section id="" class="section__4"></section>
-        `;
-    };
+        `
+    }
     render_section_4();
 
     // RENDER SECTION 4 ITEMS
     const section4Item1 = document.querySelector('.section__4');
     function section4__container1() {
         section4Item1.innerHTML += `
-        <div id="section4__container1" class="">Handbag</div>
-        `;
-    };
+        <div id="section4__container1" class=""></div>
+        `
+    }
     section4__container1();
-
-    const section41item = document.querySelector('#section4__container1');
-    function section4__container1_item() {
-        section41item.innerHTML += `
-        <div id="" class="handbag__product">Products</div>
-        `;
-    };
-    section4__container1_item();
 
     // CREATING PRODUCT ARRAY OBJECTS
     const handbagProductList = [
         {
             name: "KAMPUS TIGER BACKPACK",
+            url: "#",
+            image: "./images/section 4/KAMPUS TIGER BACKPACK.jpg",
             price: "$265",
             gender: "MALE",
             color: "BLACK",
         },
         {
             name: "KENZO SPORT BELT BAG",
+            url: "#",
+            image: "./images/section 4/KENZO SPORT BELT BAG.jpg",
             price: "$275",
             gender: "MALE",
             color: "BLACK",
         },
         {
             name: "KENZO Sport backpack with strap",
+            url: "#",
+            image: "./images/section 4/KENZO Sport backpack with strap.jpg",
             price: "$295",
             gender: "MALE",
             color: "WHITE",
         },
         {
             name: "KENZO Sport tote bag",
+            url: "#",
+            image: "./images/section 4/KENZO Sport tote bag.jpg",
             price: "$340",
             gender: "MALE",
             color: "BLACK",
         },
-    ];
+    ]
+
+    const handbagProduct = document.getElementById('section4__container1');
+
+    function render_section4_product () {
+        for (let product of handbagProductList) {
+            document.getElementById("section4__container1").innerHTML += `
+                <div class="product__item">
+                    <div class="product__image">
+                        <img src="` + product.image + `" alt="">
+                    </div>
+                    <div class="product__info">
+                        <div class="product__name">
+                            <a href="">KAMPUS TIGER BACKPACK</a>
+                        </div>
+                        <div class="product__price">` + product.price + `</div>
+                        <div class="product__color">` + product.color + `</div>
+                    </div>
+                </div>
+            `
+        }
+    }
+    render_section4_product();
+ 
 
 
-    
-};
+}
 render_main_section();
 
 // RENDER FOOTER
@@ -120,16 +142,14 @@ const mainFooter = document.querySelector('#mainFooter');
 function render_footer() {
     mainFooter.innerHTML += `
     <div id="footer"></div>
-    `;
+    `
     const contactContainer = document.querySelector('#footer');
     function render_contact_container() {
         contactContainer.innerHTML += `
         <div class="contact__container">CONTACT INFORMATION DIV</div>
         <div id="copyrightInfo">Copyright | MindX | C4EJS126</div>
-        `;
-    };
+        `
+    }
     render_contact_container();
-};
+}
 render_footer();
-
-
